01a04ff6fd1fcd3e492364aaee6be991dff468cd
Galina <baleva1990@gmail.com>
2023-09-08 13:47:59 +0300

    Adding the last part of demo.

--- 1.Intro to Node.js/demo/webServer.js
+++ 1.Intro to Node.js/demo/webServer.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('Server is called!');
-    res.write('Hello from NodeJS Server! Updated version!');
+
+    console.log(req.method);
+    console.log(req.headers.host);
+    console.log(req.url);
+
+    res.writeHead(201, {
+        'Content-Type': 'text/html'
+    })
+    res.write('<h1>Hello from NodeJS Server! Updated version!</h1>');
     res.end();
 });
 
