e7696cbb3b7e173f9a6bb37a005c182929418884
Galina <baleva1990@gmail.com>
2023-09-09 22:47:27 +0300

    "Add breed" page is added

--- 1.Intro to Node.js/01. Cat-Shelter-Workshop/index.js
+++ 1.Intro to Node.js/01. Cat-Shelter-Workshop/index.js
@@ -1,18 +1,43 @@
 const http = require('http');
-const homeHtml = require('./views/home/index');
+const { homeTemplate, catTemplate } = require('./views/home/index');
 const siteCss = require('./content/styles/site');
+const addBreedHtml = require('./views/addBreed');
+
+const cats = [
+    {
+        id: 1,
+        name: 'Navcho',
+        breed: 'Persian',
+        description: 'Very cute cat',
+    },
+    {
+        id: 2,
+        name: 'Mishi',
+        breed: 'Angora',
+        description: 'Fluffy cat',
+    },
+    {
+        id: 3,
+        name: 'Garry',
+        breed: 'Persian',
+        description: 'Fat cat',
+    },
+]
 
 const server = http.createServer(async (req, res) => {
+
     if (req.url == '/') {
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
         res.write(homeHtml);
-    } else if (req.url == '/style/')
-
-
-
-
+    } else if (req.url == '/styles/site.css') {
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+        res.write(siteCss);
+    } else if (req.url == '/cats/add-breed') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(addBreedHtml);
+    }
     res.end();
 });
 
--- 1.Intro to Node.js/01. Cat-Shelter-Workshop/views/home/index.js
+++ 1.Intro to Node.js/01. Cat-Shelter-Workshop/views/home/index.js
@@ -1,4 +1,4 @@
-module.exports = `
+exports.homeTemplate = `
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +7,7 @@ module.exports = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="/styles/site.js">
+    <link rel="stylesheet" href="./styles/site.css">
     <link rel="shortcut icon" type="image/png" href="../../content/images/pawprint.ico" />
     <title>Cat Shelter</title>
 </head>
@@ -31,57 +31,7 @@ module.exports = `
     <main>
         <section class="cats">
             <ul>
-                <li>
-                    <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="Black Cat">
-                    <h3></h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg" alt="">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Price: </span>350$</p>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <img src="https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg" alt="Black Cat">
-                    <h3>Pretty Kitty</h3>
-                    <p><span>Breed: </span>Bombay Cat</p>
-                    <p><span>Description: </span>Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.</p>
-                    <ul class="buttons">
-                        <li class="btn edit"><a href="">Change Info</a></li>
-                        <li class="btn delete"><a href="">New Home</a></li>
-                    </ul>
-                </li>
+            {{cats}}
             </ul>
         </section>
     </main>
@@ -90,3 +40,12 @@ module.exports = `
 
 </html>
 `
+exports.catTemplate = `
+    img src="{{imageUrl}}" alt="{{catName}}">
+   <h3>{{catName}}</h3>
+   <p><span>Breed: </span>{{breed}}</p>
+   <p><span>Description: </span>{{description}}</p>
+   <ul class="buttons">
+       <li class="btn edit"><a href="">Change Info</a></li>
+       <li class="btn delete"><a href="">New Home</a></li>
+    </ul>`;
