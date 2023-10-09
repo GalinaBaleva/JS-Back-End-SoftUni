b6e19a8493f207d70de8a45623c363ad5dfff0a6
Galina <baleva1990@gmail.com>
2023-10-09 22:50:03 +0200

    .

--- 2.Node.js Streams and Utilities/demo/streams/readWriteStream.js
+++ 2.Node.js Streams and Utilities/demo/streams/readWriteStream.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-const readStream = fs.createReadStream('./input.txt', { encoding: 'utf-8' });
-const writeStream = fs.createWriteStream('./output.txt');
+const readStream = fs.createReadStream(path.resolve(__dirname, './input.txt'), { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream(path.resolve(__dirname, './output.txt'));
 
 
 readStream.on('data', (chunk) => {
     writeStream.write(chunk);
 });
 
-readStream.on('end',() => {
+readStream.on('end', () => {
+    console.log('end')
     writeStream.end();
 });
