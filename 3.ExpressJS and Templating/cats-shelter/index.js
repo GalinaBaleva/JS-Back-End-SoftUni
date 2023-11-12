8082a1c62b12ba0301c43277947e397bf2e8874e
Galina <baleva1990@gmail.com>
2023-11-12 23:48:48 +0100

    partial header is added

--- 3.ExpressJS and Templating/cats-shelter/index.js
+++ 3.ExpressJS and Templating/cats-shelter/index.js
@@ -16,7 +16,20 @@ app.get('/', (req, res) => {
 const bodyParser = express.urlencoded({ extended: false });
 app.use(bodyParser);
 
+app.get('/add-breed', (req, res) => {
+    res.render('addBreed');
+});
+
+app.get('/add-cat', (req, res) => {
+    res.render('addCat');
+});
+
+
+//linking publib 
 app.use(express.static('public'));
+
+
+
 
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
